77dd4bc8b5128027d1e8a5c86fae7c6c74b80ce3
张城 <nobita@daxiongdeMBP.mshome.net>
2018-06-28 12:53:50 +0800

    a

--- src/pages/index/tracking.vue
+++ src/pages/index/tracking.vue
@@ -1,3 +1,22 @@
 <template>
   <div>s</div>
 </template>
+
+<script>
+  export default {
+
+
+  }
+
+
+</script>
+<style lang="postcss">
+  .a{
+    & .b{
+        &:hover{
+
+         }
+      }
+  }
+
+</style>

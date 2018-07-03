35744580cdfa9e979ac9866c91b3ac61537fde45
张城 <nobita@daxiongdeMBP.mshome.net>
2018-07-03 14:28:34 +0800

    commit dev_zc

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,10 +10,13 @@
 import Header from "~/components/Header.vue";
 import Menu from "~/components/Menu.vue";
 import Main from "~/components/Main.vue";
- 
 
 export default {
-  
+  mounted() {},
+  data() {
+    return {};git
+  },
+  methods: {},
   components: {
     Header,
     Menu,

586c9e7a51c0cca9c789f46cd418cd1ea15b59f7
张城 <nobita@daxiongdeMBP.mshome.net>
2018-07-05 15:14:06 +0800

    追踪报告日月报

--- src/pages/index/tracking.vue
+++ src/pages/index/tracking.vue
@@ -214,7 +214,6 @@
       :append-to-body="true"
       width="30%"
       center>
-      <span>需要注意的是内容是默认不居中的</span>
       <span slot="footer" class="dialog-footer">
     <el-button @click="DialogVisibleVal = false">取 消</el-button>
     <el-button type="primary" @click="DialogVisibleVal = false">确 定</el-button>
